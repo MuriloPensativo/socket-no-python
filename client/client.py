267a253832be499b238e4e17c86fbb99ced45b6e
Murilo Rodrigues Maia <murilo.maia62@aluno.ifce.edu.br>
2023-08-15 20:54:02 -0300

    versão 2, não está funcionando, arquivo 0 bytes

--- client/client.py
+++ client/client.py
@@ -3,8 +3,18 @@ import socket
 HOST = '127.0.0.1'
 PORT = 50000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall(str.encode('Mensagem enviada'))
-data = s.recv(1024)
-print('mensagem que voltou: ' + data.decode())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+namefile = str(input('Escreva o nome do arquivo a ser enviado: '))
+
+client.send(namefile.encode())
+
+with open(namefile, 'wb') as file:
+    while True:
+        data = client.recv(250000000)
+        if not data:
+            break
+        file.write(data)
+
+print(f'Arquivo "{namefile}" recebido.')
--- server/server.py
+++ server/server.py
@@ -3,18 +3,18 @@ import socket
 HOST = 'localhost'
 PORT = 50000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((HOST, PORT))
+server.listen(1)
 print('Aguardando conexão...')
 
-conn, address = s.accept()
+connection, address = server.accept()
 print('conectado na porta ', address)
 
-while 1:
-    data = conn.recv(1024)
-    if not data:
-        print('fechando a conexão')
-        conn.close()
-        break
-    conn.sendall(data)
+namefile = connection.recv(1024).decode()
+
+with open(namefile, 'rb') as file:
+    for data in file.readlines():
+        connection.send(data)
+    print('Envio concluído')
